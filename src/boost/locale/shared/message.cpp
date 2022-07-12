5f42ad75766dc0a186e771a86002878020a35044
Alexander Grund <Flamefire@users.noreply.github.com>
2022-07-12 12:27:06 +0200

    Update license headers
    Use the more concise format omitting the reference to the license file
    and refer to the URL only.

--- src/boost/locale/shared/message.cpp
+++ src/boost/locale/shared/message.cpp
@@ -1,10 +1,9 @@
 //
-//  Copyright (c) 2009-2015 Artyom Beilis (Tonkikh)
+// Copyright (c) 2009-2015 Artyom Beilis (Tonkikh)
 //
-//  Distributed under the Boost Software License, Version 1.0. (See
-//  accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
-//
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
 #define BOOST_LOCALE_SOURCE
 #define BOOST_DETAIL_NO_CONTAINER_FWD
 
