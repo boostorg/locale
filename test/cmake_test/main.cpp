595934f25ea64fa0fcfc2094955e11b423782698
Alexander Grund <Flamefire@users.noreply.github.com>
2022-07-12 12:27:06 +0200

    Update license headers
    Use the more concise format omitting the reference to the license file
    and refer to the URL only.

--- test/cmake_test/main.cpp
+++ test/cmake_test/main.cpp
@@ -1,10 +1,8 @@
 //
-//  Copyright (c) 2022 Alexander Grund
+// Copyright (c) 2022 Alexander Grund
 //
-//  Distributed under the Boost Software License, Version 1.0. (See
-//  accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
-//
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/locale/util.hpp>
 #include <cassert>
