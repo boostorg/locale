7aaa5a66284ec77268a06cb91a03e43d2a77df78
Alexander Grund <Flamefire@users.noreply.github.com>
2022-11-18 15:12:19 +0100

    Fix flaky test when std:time_put of the standard library fails to format
    Observed in VS2017 where the wchar_t version of the facet of he_IL.UTF-8 returns the format string unmodified.

--- test/test_std_formatting.cpp
+++ test/test_std_formatting.cpp
@@ -88,7 +88,8 @@ void test_by_char(const std::locale& l, const std::locale& lreal)
         if(!bad_parsing) {
             TEST(ss);
             double v1;
-            ss >> v1;
+            TEST(ss >> v1);
+            TEST_EQ(v1, 1043.34);
         }
 
         TEST_EQ(to_utf8(ss.str()), to_utf8(ss_ref.str()));
@@ -144,8 +145,10 @@ void test_by_char(const std::locale& l, const std::locale& lreal)
 
         std::use_facet<std::time_put<RefCharType>>(lreal)
           .put(ss_ref, ss_ref, RefCharType(' '), &tm, rfmt.c_str(), rfmt.c_str() + rfmt.size());
-
-        TEST_EQ(to_utf8(ss.str()), to_utf8(ss_ref.str()));
+        if(ss_ref.str() == rfmt)
+            std::cout << "-- Standard library failed to format the datetime value" << std::endl;
+        else
+            TEST_EQ(to_utf8(ss.str()), to_utf8(ss_ref.str()));
     }
 }
 
@@ -160,101 +163,34 @@ void test_main(int /*argc*/, char** /*argv*/)
     mgr.select("std");
     boost::locale::localization_backend_manager::global(mgr);
     boost::locale::generator gen;
-
-    {
-        std::cout << "en_US.UTF locale" << std::endl;
+    for(const std::string lName : {"en_US.UTF-8", "en_US.ISO8859-1", "he_IL.UTF-8", "he_IL.ISO8859-8"}) {
+        std::cout << lName << " locale" << std::endl;
         std::string real_name;
-        std::string name = get_std_name("en_US.UTF-8", &real_name);
+        std::string name = get_std_name(lName, &real_name);
         if(name.empty()) {
-            std::cout << "en_US.UTF-8 not supported" << std::endl;
+            std::cout << lName << " not supported" << std::endl;
         } else {
             std::locale l1 = gen(name), l2(real_name.c_str());
-            std::cout << "UTF-8" << std::endl;
-            if(name == real_name)
+            if(lName.find(".UTF-8") != std::string::npos) {
+                std::cout << "\tUTF-8" << std::endl;
+                if(name == real_name)
+                    test_by_char<char, char>(l1, l2);
+                else
+                    test_by_char<char, wchar_t>(l1, l2);
+            } else {
+                std::cout << "\tchar" << std::endl;
                 test_by_char<char, char>(l1, l2);
-            else
-                test_by_char<char, wchar_t>(l1, l2);
+            }
 
-            std::cout << "Wide UTF-" << sizeof(wchar_t) * 8 << std::endl;
+            std::cout << "\tWide UTF-" << sizeof(wchar_t) * 8 << std::endl;
             test_by_char<wchar_t, wchar_t>(l1, l2);
 
 #ifdef BOOST_LOCALE_ENABLE_CHAR16_T
-            std::cout << "char16 UTF-16" << std::endl;
+            std::cout << "\tchar16 UTF-16" << std::endl;
             test_by_char<char16_t, char16_t>(l1, l2);
 #endif
 #ifdef BOOST_LOCALE_ENABLE_CHAR32_T
-            std::cout << "char32 UTF-32" << std::endl;
-            test_by_char<char32_t, char32_t>(l1, l2);
-#endif
-        }
-    }
-    {
-        std::cout << "en_US.Latin-1 locale" << std::endl;
-        std::string real_name;
-        std::string name = get_std_name("en_US.ISO8859-1", &real_name);
-        if(name.empty()) {
-            std::cout << "en_US.ISO8859-8 not supported" << std::endl;
-        } else {
-            std::locale l1 = gen(name), l2(real_name.c_str());
-            test_by_char<char, char>(l1, l2);
-            std::cout << "Wide UTF-" << sizeof(wchar_t) * 8 << std::endl;
-            test_by_char<wchar_t, wchar_t>(l1, l2);
-
-#ifdef BOOST_LOCALE_ENABLE_CHAR16_T
-            std::cout << "char16 UTF-16" << std::endl;
-            test_by_char<char16_t, char16_t>(l1, l2);
-#endif
-#ifdef BOOST_LOCALE_ENABLE_CHAR32_T
-            std::cout << "char32 UTF-32" << std::endl;
-            test_by_char<char32_t, char32_t>(l1, l2);
-#endif
-        }
-    }
-    {
-        std::cout << "he_IL.UTF locale" << std::endl;
-        std::string real_name;
-        std::string name = get_std_name("he_IL.UTF-8", &real_name);
-        if(name.empty()) {
-            std::cout << "he_IL.UTF-8 not supported" << std::endl;
-        } else {
-            std::locale l1 = gen(name), l2(real_name.c_str());
-            std::cout << "UTF-8" << std::endl;
-            if(name == real_name)
-                test_by_char<char, char>(l1, l2);
-            else
-                test_by_char<char, wchar_t>(l1, l2);
-
-            std::cout << "Wide UTF-" << sizeof(wchar_t) * 8 << std::endl;
-            test_by_char<wchar_t, wchar_t>(l1, l2);
-
-#ifdef BOOST_LOCALE_ENABLE_CHAR16_T
-            std::cout << "char16 UTF-16" << std::endl;
-            test_by_char<char16_t, char16_t>(l1, l2);
-#endif
-#ifdef BOOST_LOCALE_ENABLE_CHAR32_T
-            std::cout << "char32 UTF-32" << std::endl;
-            test_by_char<char32_t, char32_t>(l1, l2);
-#endif
-        }
-    }
-    {
-        std::cout << "he_IL.ISO8859-8 locale" << std::endl;
-        std::string real_name;
-        std::string name = get_std_name("he_IL.ISO8859-8", &real_name);
-        if(name.empty()) {
-            std::cout << "he_IL.ISO8859-8 not supported" << std::endl;
-        } else {
-            std::locale l1 = gen(name), l2(real_name.c_str());
-            test_by_char<char, char>(l1, l2);
-            std::cout << "Wide UTF-" << sizeof(wchar_t) * 8 << std::endl;
-            test_by_char<wchar_t, wchar_t>(l1, l2);
-
-#ifdef BOOST_LOCALE_ENABLE_CHAR16_T
-            std::cout << "char16 UTF-16" << std::endl;
-            test_by_char<char16_t, char16_t>(l1, l2);
-#endif
-#ifdef BOOST_LOCALE_ENABLE_CHAR32_T
-            std::cout << "char32 UTF-32" << std::endl;
+            std::cout << "\tchar32 UTF-32" << std::endl;
             test_by_char<char32_t, char32_t>(l1, l2);
 #endif
         }
