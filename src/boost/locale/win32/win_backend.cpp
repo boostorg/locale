7b0c37d404f801d59747f406f514ca419730deeb
Alexander Grund <Flamefire@users.noreply.github.com>
2023-02-16 18:32:48 +0100

    Move `BOOST_LOCALE_SOURCE` define to buildsystem
    Ensures that this is defined consistently for all files.
    Fixes #145

--- src/boost/locale/win32/win_backend.cpp
+++ src/boost/locale/win32/win_backend.cpp
@@ -4,7 +4,6 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#define BOOST_LOCALE_SOURCE
 #include "boost/locale/win32/win_backend.hpp"
 #include <boost/locale/gnu_gettext.hpp>
 #include <boost/locale/info.hpp>
