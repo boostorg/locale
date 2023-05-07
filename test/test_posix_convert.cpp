2ec633a93e58d431b800e03894f7c52768868df3
Alexander Grund <Flamefire@users.noreply.github.com>
2023-05-07 13:00:17 +0200

    Add tests for failing `create_simple_converter` and fix missing coverage

--- test/test_posix_convert.cpp
+++ test/test_posix_convert.cpp
@@ -26,18 +26,14 @@ void test_char()
     test_one<CharType>(l, "Hello World i", "hello world i", "HELLO WORLD I");
 
     std::string name = "en_US.UTF-8";
-    if(!has_posix_locale(name))
-        std::cout << "- " << name << " is not supported, skipping" << std::endl;
-    else {
+    if(has_posix_locale(name)) {
         std::cout << "- Testing " << name << std::endl;
         l = gen(name);
         test_one<CharType>(l, "Façade", "façade", "FAÇADE");
     }
 
     name = "en_US.ISO8859-1";
-    if(!has_posix_locale(name))
-        std::cout << "- " << name << " is not supported, skipping" << std::endl;
-    else {
+    if(has_posix_locale(name)) {
         std::cout << "Testing " << name << std::endl;
         l = gen(name);
         test_one<CharType>(l, "Hello World", "hello world", "HELLO WORLD");
@@ -53,7 +49,7 @@ void test_char()
     else {
         std::cout << "Testing " << name << std::endl;
         locale_holder cl(newlocale(LC_ALL_MASK, name.c_str(), nullptr));
-        TEST(cl);
+        TEST_REQUIRE(cl);
 #ifndef BOOST_LOCALE_NO_POSIX_BACKEND
         if(towupper_l(L'i', cl) == 0x130)
             test_one<CharType>(gen(name), "i", "i", "İ");
