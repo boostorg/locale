21ddbb006de857b25a5d4b8805a453d4613acdfc
Alexander Grund <alexander.grund@tu-dresden.de>
2023-09-12 17:00:12 +0200

    DEBUG: echo stdlib

--- src/boost/locale/icu/conversion.cpp
+++ src/boost/locale/icu/conversion.cpp
@@ -9,6 +9,7 @@
 #include "boost/locale/icu/cdata.hpp"
 #include "boost/locale/icu/icu_util.hpp"
 #include "boost/locale/icu/uconv.hpp"
+#include <boost/config/pragma_message.hpp>
 #include <limits>
 #include <unicode/locid.h>
 #include <unicode/normlzr.h>
@@ -131,6 +132,7 @@ namespace boost { namespace locale { namespace impl_icu {
     private:
         UCaseMap* map_;
     };
+    BOOST_PRAGMA_MESSAGE("Stdlib: " BOOST_STDLIB)
 
     template<typename U8Char>
     class utf8_converter_impl : public converter<U8Char> {
