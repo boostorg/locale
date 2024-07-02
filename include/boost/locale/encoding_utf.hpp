6ddae0ae4f0ddf6494e3ef2bb4267d6a28d73931
Alexander Grund <Flamefire@users.noreply.github.com>
2024-07-02 16:26:27 +0200

    Format sources

--- include/boost/locale/encoding_utf.hpp
+++ include/boost/locale/encoding_utf.hpp
@@ -28,7 +28,8 @@ namespace boost { namespace locale { namespace conv {
     ///
     /// \throws conversion_error: Conversion failed (e.g. \a how is \c stop and any character cannot be decoded)
     template<typename CharOut, typename CharIn, typename TAlloc = std::allocator<CharOut>>
-    std::basic_string<CharOut, std::char_traits<CharOut>, TAlloc> utf_to_utf(const CharIn* begin, const CharIn* end, method_type how = default_method, const TAlloc& alloc = TAlloc())
+    std::basic_string<CharOut, std::char_traits<CharOut>, TAlloc>
+    utf_to_utf(const CharIn* begin, const CharIn* end, method_type how = default_method, const TAlloc& alloc = TAlloc())
     {
         std::basic_string<CharOut, std::char_traits<CharOut>, TAlloc> result(alloc);
         result.reserve(end - begin);
@@ -48,7 +49,8 @@ namespace boost { namespace locale { namespace conv {
     ///
     /// \throws conversion_error: Conversion failed (e.g. \a how is \c stop and any character cannot be decoded)
     template<typename CharOut, typename CharIn, typename TAlloc = std::allocator<CharOut>>
-    std::basic_string<CharOut, std::char_traits<CharOut>, TAlloc> utf_to_utf(const CharIn* str, method_type how = default_method, const TAlloc& alloc = TAlloc())
+    std::basic_string<CharOut, std::char_traits<CharOut>, TAlloc>
+    utf_to_utf(const CharIn* str, method_type how = default_method, const TAlloc& alloc = TAlloc())
     {
         return utf_to_utf<CharOut, CharIn, TAlloc>(str, util::str_end(str), how, alloc);
     }
@@ -57,9 +59,18 @@ namespace boost { namespace locale { namespace conv {
     ///
     /// \throws conversion_error: Conversion failed (e.g. \a how is \c stop and any character cannot be decoded)
     template<typename CharOut, typename CharIn, typename TAlloc>
-    typename std::enable_if<std::is_same<CharIn, typename TAlloc::value_type>::value, std::basic_string<CharOut, std::char_traits<CharOut>, typename std::allocator_traits<TAlloc>::template rebind_alloc<CharOut>>>::type utf_to_utf(const std::basic_string<CharIn, std::char_traits<CharIn>, TAlloc>& str, method_type how = default_method)
+    typename std::enable_if<
+      std::is_same<CharIn, typename TAlloc::value_type>::value,
+      std::basic_string<CharOut,
+                        std::char_traits<CharOut>,
+                        typename std::allocator_traits<TAlloc>::template rebind_alloc<CharOut>>>::type
+    utf_to_utf(const std::basic_string<CharIn, std::char_traits<CharIn>, TAlloc>& str, method_type how = default_method)
     {
-        return utf_to_utf<CharOut, CharIn, typename std::allocator_traits<TAlloc>::template rebind_alloc<CharOut>>(str.c_str(), str.c_str() + str.size(), how, typename std::allocator_traits<TAlloc>::template rebind_alloc<CharOut>(str.get_allocator()));
+        return utf_to_utf<CharOut, CharIn, typename std::allocator_traits<TAlloc>::template rebind_alloc<CharOut>>(
+          str.c_str(),
+          str.c_str() + str.size(),
+          how,
+          typename std::allocator_traits<TAlloc>::template rebind_alloc<CharOut>(str.get_allocator()));
     }
 
     /// @}
