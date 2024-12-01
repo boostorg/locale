1d85ae3162a13bd8e8e9b3ca87423dd76a009a87
Alexander Grund <Flamefire@users.noreply.github.com>
2024-12-01 18:23:54 +0100

    Add support for ISO-15924 abbreviation script code to `locale_data`
    The value is (currently) ignored but this allows to parse locale names
    returned by ICU.

--- src/boost/locale/util/locale_data.cpp
+++ src/boost/locale/util/locale_data.cpp
@@ -1,6 +1,6 @@
 //
 // Copyright (c) 2009-2011 Artyom Beilis (Tonkikh)
-// Copyright (c) 2022-2023 Alexander Grund
+// Copyright (c) 2022-2024 Alexander Grund
 //
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
@@ -14,6 +14,26 @@
 #include <string>
 
 namespace boost { namespace locale { namespace util {
+    /// Convert uppercase ASCII to lower case, return true if converted
+    static bool make_lower(char& c)
+    {
+        if(is_upper_ascii(c)) {
+            c += 'a' - 'A';
+            return true;
+        } else
+            return false;
+    }
+
+    /// Convert lowercase ASCII to upper case, return true if converted
+    static bool make_upper(char& c)
+    {
+        if(is_lower_ascii(c)) {
+            c += 'A' - 'a';
+            return true;
+        } else
+            return false;
+    }
+
     locale_data::locale_data()
     {
         reset();
@@ -28,6 +48,7 @@ namespace boost { namespace locale { namespace util {
     void locale_data::reset()
     {
         language_ = "C";
+        script_.clear();
         country_.clear();
         encoding_ = "US-ASCII";
         variant_.clear();
@@ -37,6 +58,8 @@ namespace boost { namespace locale { namespace util {
     std::string locale_data::to_string() const
     {
         std::string result = language_;
+        if(!script_.empty())
+            (result += '_') += script_;
         if(!country_.empty())
             (result += '_') += country_;
         if(!encoding_.empty() && !util::are_encodings_equal(encoding_, "US-ASCII"))
@@ -60,13 +83,38 @@ namespace boost { namespace locale { namespace util {
             return false;
         // lowercase ASCII
         for(char& c : tmp) {
-            if(is_upper_ascii(c))
-                c += 'a' - 'A';
-            else if(!is_lower_ascii(c))
+            if(!is_lower_ascii(c) && !make_lower(c))
                 return false;
         }
         if(tmp != "c" && tmp != "posix") // Keep default
             language_ = tmp;
+
+        if(end >= input.size())
+            return true;
+        else if(input[end] == '-' || input[end] == '_')
+            return parse_from_script(input.substr(end + 1));
+        else if(input[end] == '.')
+            return parse_from_encoding(input.substr(end + 1));
+        else {
+            BOOST_ASSERT_MSG(input[end] == '@', "Unexpected delimiter");
+            return parse_from_variant(input.substr(end + 1));
+        }
+    }
+
+    bool locale_data::parse_from_script(const std::string& input)
+    {
+        const auto end = input.find_first_of("-_@.");
+        std::string tmp = input.substr(0, end);
+        // Script is exactly 4 ASCII characters, otherwise it is not present
+        if(tmp.length() != 4)
+            return parse_from_country(input);
+
+        for(char& c : tmp) {
+            if(!is_lower_ascii(c) && !make_lower(c))
+                return parse_from_country(input);
+        }
+        make_upper(tmp[0]); // Capitalize first letter only
+        script_ = tmp;
 
         if(end >= input.size())
             return true;
@@ -91,10 +139,9 @@ namespace boost { namespace locale { namespace util {
             return false;
 
         // Make uppercase
-        for(char& c : tmp) {
-            if(util::is_lower_ascii(c))
-                c += 'A' - 'a';
-        }
+        for(char& c : tmp)
+            make_upper(c);
+
         // If it's ALL uppercase ASCII, assume ISO 3166 country id
         if(std::find_if_not(tmp.begin(), tmp.end(), util::is_upper_ascii) != tmp.end()) {
             // else handle special cases:
@@ -142,20 +189,16 @@ namespace boost { namespace locale { namespace util {
             return false;
         variant_ = input;
         // No assumptions, just make it lowercase
-        for(char& c : variant_) {
-            if(util::is_upper_ascii(c))
-                c += 'a' - 'A';
-        }
+        for(char& c : variant_)
+            make_lower(c);
         return true;
     }
 
     locale_data& locale_data::encoding(std::string new_encoding, const bool uppercase)
     {
         if(uppercase) {
-            for(char& c : new_encoding) {
-                if(util::is_lower_ascii(c))
-                    c += 'A' - 'a';
-            }
+            for(char& c : new_encoding)
+                make_upper(c);
         }
         encoding_ = std::move(new_encoding);
         utf8_ = util::normalize_encoding(encoding_) == "utf8";
